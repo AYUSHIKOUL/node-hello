8de14a0bf802e1fea297fa92284f9c0b8a65dfab
AYUSHI <koulayushi1999@gmail.com>
2022-10-03 17:19:32 +0530

    test4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node-js website Testing jenkins-101\n'
+  const msg = ' Node-js website Testing jenkins-101\n'
   res.end(msg);
 });
 
